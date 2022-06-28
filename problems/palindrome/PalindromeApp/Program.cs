cdfe326b6f0c1797bf23093c19755ccc0064cf7a
Samuel Camilo <scamilo.dev@gmail.com>
2022-06-27 23:21:39 -0300

    FEAT:Resolve palindrome problem

---
+++ problems/palindrome/PalindromeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace PalindromeApp 
+{
+    class Program
+    {
+        static string FormatStringInput(string value)
+        {
+            Regex reg = new Regex("[*'\",_&#@-]");
+            return reg.Replace(value, string.Empty)
+                      .Replace(" ", string.Empty)
+                      .ToLower();
+        }
+
+        static bool CheckIfIsPalindromeInput(string input)
+            => input.Equals(string.Join(string.Empty, input.Reverse()));
+
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine() ?? string.Empty;
+            bool result = CheckIfIsPalindromeInput(FormatStringInput(input));
+
+            Console.WriteLine(result ? "Is a Palindrome!" : "Isn´t a Palindrome!");
+        }
+    }
+}
